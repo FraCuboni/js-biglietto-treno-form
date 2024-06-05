2f988f2ffe2e8bd54b509b6705d465e10c06b96c
FraCuboni <fracuboni@gmail.com>
2024-06-05 16:24:28 +0200

    added age nameinout and kminput

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 // input
 const nameInput = document.querySelector(".nameinput");
+const kmInput = document.querySelector(".kminput");
 
 
 
@@ -12,69 +13,71 @@ sendButton.addEventListener("click",
 
     function(){
         let nameValue = nameInput.value;
-        console.log("Nome inserito:", nameValue);
+        let kmValue = parseInt(kmInput.value, 10);
+        console.log("Nome inserito:", nameValue, "chilometraggio inserito:", kmValue);
+
+
+        
+        console.log("il chilometraggio dell utente è:",kmValue);
+
 
     }
 );
 
 
 
-// if(giorgio){
-//     // stabilisco il prezzo al kilometro
-    
-//     const prezzoAlKm = 0.21;
-    
-//     // stabilisco sconto under18
-    
-//     const sconto18 = 0.80;
-    
-//     // stabilisco sconto over65
-    
-//     const sconto65 = 0.60;
-    
-//     // chiedo il chilometraggio
-    
-//     let chilometraggio = parseInt(prompt("quanti Km devi viaggiare?"));
-//     console.log("il chilometraggio scelto dall'utente è:",chilometraggio);
-    
-//     // calcolo il prezzo in base alla distanza
-    
-//     let prezzoKm = prezzoAlKm * chilometraggio;
-//     console.log("Il prezzo per il chilometraggio selezionato è:", prezzoKm);
-    
-//     // chiedo l'età
-    
-//     let etaUtente = parseInt(prompt("quanti anni hai?"));
-//     console.log("l'età dell utente è:",etaUtente);
-    
-//     // calcolo se applicare sconti per l'età
-    
-//     if(etaUtente<18){
-    
-//         prezzoKm = prezzoKm * sconto18;
-        
-       
-//     }else if(etaUtente>=65){
-    
-//         prezzoKm = prezzoKm * sconto65;
-        
-    
-//     }
-    
-//     console.log("Il prezzo dopo l'applicazione di eventuali sconti e privo di tagli di decimali è", prezzoKm);
-    
-//     // taglio i decimali in eccesso
-    
-//     let prezzoFinale = Math.round(prezzoKm * 100) / 100;
-    
-//     // Do il prezzo finale
-    
-//     console.log("Il prezzo finale è",prezzoFinale);
+
+// stabilisco il prezzo al kilometro
+
+const prezzoAlKm = 0.21;
+
+// stabilisco sconto under18
+
+const sconto18 = 0.80;
+
+// stabilisco sconto over65
+
+const sconto65 = 0.60;
+
+// chiedo il chilometraggio
+
+let chilometraggio = 10;
+console.log("il chilometraggio scelto dall'utente è:",chilometraggio);
+
+// calcolo il prezzo in base alla distanza
+
+let prezzoKm = prezzoAlKm * chilometraggio;
+console.log("Il prezzo per il chilometraggio selezionato è:", prezzoKm);
+
+// chiedo l'età
+
+
+// calcolo se applicare sconti per l'età
+
+// if(etaUtente<18){
+
+//     prezzoKm = prezzoKm * sconto18;
     
     
-    
-    
-    
+// }else if(etaUtente>=65){
+
+//     prezzoKm = prezzoKm * sconto65;
     
 
 // }
+
+// console.log("Il prezzo dopo l'applicazione di eventuali sconti e privo di tagli di decimali è", prezzoKm);
+
+// // taglio i decimali in eccesso
+
+// let prezzoFinale = Math.round(prezzoKm * 100) / 100;
+
+// // Do il prezzo finale
+
+// console.log("Il prezzo finale è",prezzoFinale);
+    
+    
+    
+    
+    
+    
